91f3d13f15d16470d22dbe76090a9e444853a7d2
Tim S <tim.seegert@student.htw-berlin.de>
2023-04-27 11:57:18 +0300

    skeleton is ready and register and welcome page are finished

---
+++ frontend/src/components/form/mainForm.tsx
@@ -0,0 +1,28 @@
+import './mainForm.css'
+import MainButton from '../button/mainButton';
+import Form from 'react-bootstrap/Form';
+
+const MainForm = (props: any) => {
+  return (
+    <div >
+      <h2 className='fontStyle'>{props.heading}</h2>
+      <div className='fontStyle' style={{marginBottom: '5px'}}>{props.subHeadline}</div>
+      <Form>
+        {props.children}
+        {
+          props.checkboxText 
+          ? <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" 
+              label={props.checkboxText} 
+              style={{fontFamily: 'SF Pro Text'}}
+              onChange={props.checkBoxFunction}/>
+            </Form.Group>
+          : <></>
+        }
+      </Form>
+      <MainButton buttonText={props.buttonText} handleClick={props.submitFunction}></MainButton>
+    </div>
+  )
+}
+
+export default MainForm
